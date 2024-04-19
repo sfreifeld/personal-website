06ec80ec4e0c6577b586e08b758b0f3f72a6023f
Sabrina Freifeld <sfrei.1996@gmail.com>
2024-04-19 13:21:48 -0600

    new background

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,8 +1,18 @@
-import homeBackgroundVideo from "../assets/home-background.mp4"
+import React from 'react';
+import Lottie from 'react-lottie';
+import animationData from '../assets/animation2.json'; // Ensure this is the correct path to your Lottie file
 import ContactForm from '../Components/ContactForm';
 
-function Contact() {
 
+function Contact() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true, 
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice' // Adjust as needed
+    }
+  };
 
   return (
     <div className="relative isolate bg-white">
@@ -10,10 +20,10 @@ function Contact() {
         <div className="relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
           <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
             <div className="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden bg-gray-100  ring-gray-900/10 lg:w-1/2">
-            <video autoPlay loop muted className="absolute w-full h-full object-cover">
-              <source src={homeBackgroundVideo} type="video/mp4" />
-              Your browser does not support the video tag.
-              </video>
+            <div id="video-background" className="absolute top-0 left-0 w-full h-full z-[-1]">
+                {/* Lottie animation as background */}
+                <Lottie options={defaultOptions} height="100%" width="100%" />
+              </div>
             </div>
             <h2 className="text-3xl font-bold tracking-tight text-gray-800  font-heading">Get in touch!</h2>
             <p className="mt-6 text-lg leading-8 text-gray-600 font-paragraph">
